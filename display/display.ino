9781fa2eab3ce0793a0a6d22c25132232b11e3f5
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-03-24 22:23:41 -0700

    added more bitmaps to display code

--- display/display.ino
+++ display/display.ino
@@ -3,7 +3,8 @@
 #include <I2C_LCD.h>
 I2C_LCD LCD;
 uint8_t I2C_LCD_ADDRESS = 0x51;
-//extern GUI_Bitmap_t bmBruin;
+extern GUI_Bitmap_t bmBruinRacing;
+extern GUI_Bitmap_t bmBearHead;
 const int refreshPeriod = 100; // display refresh time [ms]
 int lastRefreshTime(0);
 
@@ -11,7 +12,7 @@ int lastRefreshTime(0);
 const int pinButton1 = 13;
 const int pinButton2 = 12;
 int mode = 0;
-char mode_names[2][7] = {"power ", "torque"};
+char mode_names[2][7] = {"POWER ", "TORQUE"};
 
 void setup() {
   // configure button pins
@@ -21,19 +22,21 @@ void setup() {
   // init I2C interface
   Wire.begin();
   // configure LCD for boot logo OFF, backlight ON, bitmap display mode
-//  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
+  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
 
   // clear screen and display sweaty bruin
-//  LCD.CleanAll(WHITE);
-//  LCD.DrawScreenAreaAt(&bmBruin, 0, 0);
-//  delay(3000);
+  LCD.CleanAll(WHITE);
+  LCD.DrawScreenAreaAt(&bmBruinRacing, 0, 1);
+  delay(3000);
   LCD.CleanAll(WHITE);
 
   // configure LCD for character display mode , 8x16 font, auto-newline, black text with background
+  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
   LCD.WorkingModeConf(OFF, ON, WM_CharMode);
-  LCD.CharGotoXY(0,0);
-  LCD.FontModeConf(Font_8x16_1, FM_ANL_AAA, BLACK_BAC);
-  LCD.print("mode: ");
+  LCD.FontModeConf(Font_6x8, FM_MNL_AAA, BLACK_NO_BAC);
+  LCD.DispStringAt("MODE:", 68, 5);
+  LCD.DispStringAt("RPM:", 68, 35);
+  LCD.FontModeConf(Font_8x16_2, FM_MNL_AAA, BLACK_BAC);
 }
 
 void loop() {
@@ -53,6 +56,7 @@ void loop() {
 }
 
 void update_display() {
-  LCD.DispStringAt(mode_names[mode], 45, 0);
+  LCD.DispStringAt(mode_names[mode], 78, 15);
+  LCD.DispStringAt("3600", 78, 45);
 }
 
