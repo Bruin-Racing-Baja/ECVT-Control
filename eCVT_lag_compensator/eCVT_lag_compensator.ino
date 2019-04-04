a3d7a429cd341bf772a4d75de47dc6af95fd6ea0
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-04-04 07:51:48 -0700

    added button debouncing to lag compensator

--- eCVT_lag_compensator/eCVT_lag_compensator.ino
+++ eCVT_lag_compensator/eCVT_lag_compensator.ino
@@ -7,10 +7,23 @@
 #include <Servo.h>
 
 // buttons
+#define DEBOUNCE_DELAY 50
 const int button1_pin = 13;
+int button1_state = HIGH;
+int button1_last_state = HIGH;
+int button1_last_debounce(0);
 const int button2_pin = 12;
+int button2_state = HIGH;
+int button2_last_state = HIGH;
+int button2_last_debounce(0);
 const int button3_pin = 11;
+int button3_state = HIGH;
+int button3_last_state = HIGH;
+int button3_last_debounce(0);
 const int button4_pin = 10;
+int button4_state = HIGH;
+int button4_last_state = HIGH;
+int button4_last_debounce(0);
 
 // PWM constants
 #define PW_STOP 1510
@@ -48,7 +61,7 @@ int lastControlTime(0);
 
 // hall effect sensor
 #define NUM_MAGNETS 1
-const int sensor_pin = 7;
+const int sensor_pin = 3;
 int trigger_time(0);
 int last_trigger(0);
 int rpm(0);
@@ -107,7 +120,8 @@ void control_function() {
 
   // compute control signal and write to actuator
   u_k = A*e_k + B*e_k1 - C*e_k1;
-  Actuator.writeMicroseconds(u_k + PW_STOP);
+  int u_k_final = max(min(u_k, u_k_max), u_k_min);
+  Actuator.writeMicroseconds(u_k_final + PW_STOP);
 
   // update past values
   u_k1 = u_k;
@@ -130,11 +144,35 @@ void loop() {
   attachInterrupt(digitalPinToInterrupt(sensor_pin), hall_effect_interrupt, FALLING);
 
   // check button presses
-  if (digitalRead(button3_pin) == HIGH) {
+  button1_state = debounce_button(digitalRead(button1_pin), button1_state, button1_last_state, button1_last_debounce); 
+  button2_state = debounce_button(digitalRead(button2_pin), button2_state, button2_last_state, button2_last_debounce);
+  button3_state = debounce_button(digitalRead(button3_pin), button3_state, button3_last_state, button3_last_debounce);
+  button4_state = debounce_button(digitalRead(button4_pin), button4_state, button4_last_state, button4_last_debounce);
+  
+  // update reference value
+  if (button1_state == LOW) {
     r_k = MAX_TORQUE;
-  } else if (digitalRead(button4_pin) == HIGH) {
+  }
+  if (button2_state == LOW) {
     r_k = MAX_POWER;
-  } else if (digitalRead(button2_pin) == HIGH) {
+  }
+  if (button3_state == LOW) {
     r_k = 5000;
   }
+}
+
+bool debounce_button(const int reading, int &button_state, int &last_button_state , int &last_debounce) {
+  if (reading != last_button_state) {
+    last_debounce = millis();
+  }
+
+  if ((millis() - last_debounce) > DEBOUNCE_DELAY) {
+
+    if (reading != button_state) {
+      button_state = reading;
+    }
+  }
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  last_button_state = reading;
 }
