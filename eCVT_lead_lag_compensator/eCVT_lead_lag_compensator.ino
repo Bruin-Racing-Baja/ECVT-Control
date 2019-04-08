b4cb4afc79e38ba2f4c0a5f4d6d0f69b8e526d47
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-04-07 23:58:01 -0700

    commented out display code in controller

--- eCVT_lead_lag_compensator/eCVT_lead_lag_compensator.ino
+++ eCVT_lead_lag_compensator/eCVT_lead_lag_compensator.ino
@@ -11,14 +11,14 @@
 #include <Servo.h>
 
 // display setup
-#include <Wire.h>
-#include <I2C_LCD.h>
-I2C_LCD LCD;
-uint8_t I2C_LCD_ADDRESS = 0x51;
-extern GUI_Bitmap_t bmBruinRacing;
-extern GUI_Bitmap_t bmBearHead;
-const int refreshPeriod = 1000; // display refresh time [ms]
-int lastRefreshTime(0);
+//#include <Wire.h>
+//#include <I2C_LCD.h>
+//I2C_LCD LCD;
+//uint8_t I2C_LCD_ADDRESS = 0x51;
+//extern GUI_Bitmap_t bmBruinRacing;
+//extern GUI_Bitmap_t bmBearHead;
+//const int refreshPeriod = 1000; // display refresh time [ms]
+//int lastRefreshTime(0);
 
 // buttons
 const byte button1_pin = 13;
@@ -32,19 +32,21 @@ char mode_names[3][7] = {"TORQUE", "POWER", "LAUNCH"};
 #define PW_STOP 1510
 #define PW_MIN 1000
 #define PW_MAX 2000
+#define u_k_limit 100
 const int u_k_min = PW_MIN - PW_STOP;
 const int u_k_max = PW_MAX - PW_STOP;
 
 // reference signals
-#define MAX_TORQUE 2700
-#define MAX_POWER 3400
+#define MAX_TORQUE 2750
+#define MAX_POWER 3600
 
 // actuator
 Servo Actuator;
 const byte actuator_pin = 9;
 #define POT_MARGIN 10
-#define POT_MIN 146 + POT_MARGIN
-#define POT_MAX 762 - POT_MARGIN
+int POT_MIN = 125 + POT_MARGIN;
+int POT_MAX = 530 - POT_MARGIN;
+int POT_ENGAGE = 509;
 const byte pot_pin = A0;
 int current_pos(0);
 
@@ -58,7 +60,7 @@ int u_k(0);
 int u_k1(0);
 const byte controlPeriod = 20; // [ms]
 const double Ts = controlPeriod/1000.0; // controlPeriod [s]
-const byte K = 5; // controller gain
+const byte K = 25; // controller gain
 const double lead_z = 4.4212; // lead compensator zero
 const double lead_p = 61.579; // lead compensator pole
 const double lead_A = K*(lead_z+2/Ts)/(lead_p+2/Ts); // multiplied by e_k
@@ -79,7 +81,6 @@ unsigned long last_trigger(0);
 unsigned long delta_t(0);
 unsigned int rpm(0);
 double HighLow(0);
-double RPMCount(0);
 
 void setup() {
 
@@ -87,13 +88,13 @@ void setup() {
   Serial.begin(9600);
 
   // init I2C interface
-  Wire.begin();
-  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
+//  Wire.begin();
+//  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
 
   // clear screen and display sweaty bruin
-  LCD.CleanAll(WHITE);
-  LCD.DrawScreenAreaAt(&bmBruinRacing, 0, 1);
-  delay(1000);
+//  LCD.CleanAll(WHITE);
+//  LCD.DrawScreenAreaAt(&bmBruinRacing, 0, 1);
+//  delay(1000);
   
   // setup buttons
   pinMode(button1_pin, INPUT);
@@ -115,12 +116,12 @@ void setup() {
 //  last_trigger = trigger_time;
 
   // configure LCD to write text
-  LCD.CleanAll(WHITE);
-  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
-  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
-  LCD.FontModeConf(Font_6x8, FM_MNL_AAA, BLACK_NO_BAC);
-  LCD.DispStringAt("MODE:", 68, 5);
-  LCD.DispStringAt("RPM:", 68, 35);
+//  LCD.CleanAll(WHITE);
+//  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
+//  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
+//  LCD.FontModeConf(Font_6x8, FM_MNL_AAA, BLACK_NO_BAC);
+//  LCD.DispStringAt("MODE:", 68, 5);
+//  LCD.DispStringAt("RPM:", 68, 35);
 //  LCD.FontModeConf(Font_8x16_2, FM_MNL_AAA, BLACK_BAC);
 
   // create timer interrupt
@@ -139,13 +140,15 @@ SIGNAL(TIMER0_COMPA_vect) {
     control_function();
     lastControlTime = current_millis;
 
+    Serial.print(r_k);
+    Serial.print(" ");
     Serial.print(u_k);
     Serial.print(" ");
     Serial.print(rpm);
     Serial.print(" ");
     Serial.print(current_pos);
-    Serial.print(" ");
-    Serial.print(millis());
+//    Serial.print(" ");
+//    Serial.print(millis());
     Serial.print("\n");
   }
 }
@@ -159,7 +162,21 @@ void control_function() {
   lead_u_k = lead_A*e_k + lead_B*e_k1 - lead_C*lead_u_k1;
   u_k = lag_A*lead_u_k + lag_B*lead_u_k1 - lag_C*u_k1;
 //  u_k = max(min(u_k, u_k_max), u_k_min);
-  u_k = constrain(u_k, u_k_min, u_k_max); 
+  u_k = constrain(u_k, u_k_min, u_k_max);
+
+  if (rpm < MAX_TORQUE) {
+    POT_MAX = 530 - POT_MARGIN;
+  } else {
+    POT_MAX = POT_ENGAGE + POT_MARGIN;
+  }
+
+  // check actuator limits
+  current_pos = analogRead(pot_pin);
+  if (current_pos >= POT_MAX) {
+    u_k = -u_k_limit;
+  } else if (current_pos <= POT_MIN) {
+    u_k = u_k_limit;
+  }
 
   // write to actuator
   Actuator.writeMicroseconds(u_k + PW_STOP);
@@ -175,10 +192,9 @@ void RPMCalc() {
     if (HighLow == 1) {
       trigger_time = millis();
       unsigned short new_rpm = 60000.0 / (trigger_time - last_trigger);
-      if (new_rpm > 0 && new_rpm < 5000) {
+      if (new_rpm >= 0 && new_rpm < 5000) {
         rpm = new_rpm;
       }
-      RPMCount = 0;
       last_trigger = trigger_time;
     }
     HighLow = 0;
@@ -188,24 +204,19 @@ void RPMCalc() {
   }
 }
 
-void update_display() {
-  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
-  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
-  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
-  LCD.DispStringAt("MODE:", 68, 5);
-  LCD.DispStringAt("RPM:", 68, 35);
-  LCD.DispStringAt(mode_names[mode], 78, 15);
-  char rpm_str[5];
-  itoa(rpm, rpm_str, 10);
-  LCD.DispStringAt(rpm_str, 78, 45);
-}
+//void update_display() {
+//  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
+//  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
+//  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
+//  LCD.DispStringAt("MODE:", 68, 5);
+//  LCD.DispStringAt("RPM:", 68, 35);
+//  LCD.DispStringAt(mode_names[mode], 78, 15);
+//  char rpm_str[5];
+//  itoa(rpm, rpm_str, 10);
+//  LCD.DispStringAt(rpm_str, 78, 45);
+//}
 
 void loop() {
-  // check actuator limits
-  current_pos = analogRead(pot_pin);
-  if ((current_pos >= POT_MAX && u_k > 0) || (current_pos <= POT_MIN && u_k < 0)) {
-    u_k = 0;
-  }
 
   // check rpm
   RPMCalc();
@@ -221,9 +232,9 @@ void loop() {
     r_k = 5000;
   }
 
-  int current_millis = millis();
-  if (current_millis - lastRefreshTime >= refreshPeriod) {
-    update_display();
-    lastRefreshTime = current_millis;
-  }
+//  int current_millis = millis();
+//  if (current_millis - lastRefreshTime >= refreshPeriod) {
+//    update_display();
+//    lastRefreshTime = current_millis;
+//  }
 }
