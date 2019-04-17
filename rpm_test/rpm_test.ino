f6eff2a6a42559faf0b8726b73282e7ba3eb64a4
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-04-16 21:44:09 -0700

    updated rpm_test to include moving average reading

--- rpm_test/rpm_test.ino
+++ rpm_test/rpm_test.ino
@@ -5,10 +5,27 @@ unsigned long last_trigger(0);
 unsigned long trigger_time(0);
 int HighLow = LOW;
 bool RPMCount = false;
+int index(0);
+const int num_readings = 5;
+unsigned int readings[num_readings];
 
 
+void init_readings() {
+  for (int i = 0; i < num_readings; i++) {
+    readings[i] = 0;
+  }
+}
+
+unsigned int rpm_average() {
+  unsigned int sum = 0;
+  for (int i = 0; i < num_readings; i++) {
+    sum += readings[i];
+  }
+  return (sum / num_readings);
+}
 
 void setup() {
+  init_readings();
   pinMode(sensor_pin, INPUT);
   Serial.begin(9600);
   last_trigger = millis();
@@ -22,17 +39,15 @@ void loop(){
 void RPMCalc() {
   if (digitalRead(sensor_pin) == LOW) {
     if (HighLow == 1) {
-      RPMCount = 1;
+      trigger_time = millis();
+      rpm = 60000.0 / (trigger_time - last_trigger);
+      readings[index] = rpm;
+      index = (index + 1) % num_readings;
+      last_trigger = trigger_time;
     }
     HighLow = 0;
   }
   if (digitalRead(sensor_pin) == HIGH) {
     HighLow = 1;
-  }
-  if (RPMCount == 1) {
-    trigger_time = millis();
-    rpm = 60000.0 / (trigger_time - last_trigger);
-    RPMCount = 0;
-    last_trigger = trigger_time;
   }
 }
