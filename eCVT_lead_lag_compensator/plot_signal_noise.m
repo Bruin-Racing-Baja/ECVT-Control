6612582e7029be4e69bb42e2a6132f593a020343
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-04-28 11:49:08 -0700

    added a matlab function to plot signal noise amplitude vs frequency

--- eCVT_lead_lag_compensator/plot_signal_noise.m
+++ eCVT_lead_lag_compensator/plot_signal_noise.m
@@ -1,4 +1,4 @@
-function plot_signal_noise(data, fs)
+function [f, Pxx] = plot_signal_noise(data, fs)
 data = data - mean(data);
 Nfft = length(data);
 [Pxx, f] = pwelch(data, gausswin(Nfft), Nfft/2, Nfft, fs);
