55bf3ed5a05a5e46d1e27f5f74a1fc926fa52e73
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-04-28 11:53:19 -0700

    updated actuator_step_response.ino to be used as a simple shift in and shift out command

--- actuator_step_response/actuator_step_response.ino
+++ actuator_step_response/actuator_step_response.ino
@@ -4,30 +4,26 @@
 #define UK_MAX 2000
 #define UK_MIN 1000
 
-int uk = UK_STOP;
+int uk = 1560;
 
 const int pin_pot = A0;
 const int pin_actuator = 9;
 
 Servo Actuator;
 
-void write_command() {
-  uk = UK_MIN;
-}
-
 void setup() {
   pinMode(pin_pot, INPUT);
   Actuator.attach(pin_actuator, UK_MIN, UK_MAX);
-  Actuator.writeMicroseconds(UK_STOP);
+//  Actuator.writeMicroseconds(UK_STOP);
   Serial.begin(9600);
 
-  attachInterrupt(digitalPinToInterrupt(3), write_command, RISING);
+//  attachInterrupt(digitalPinToInterrupt(3), write_command, RISING);
 }
 
 void loop() {
   Actuator.writeMicroseconds(uk);
-  Serial.print(uk);
-  Serial.print(" ");
+//  Serial.print(uk);
+//  Serial.print(" ");
   Serial.println(analogRead(pin_pot));
   delay(20);
 }
