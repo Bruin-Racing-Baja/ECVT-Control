79bcd141fa8d1f4137537e03b1e63c3c6d12be04
Tyler McCown <tylermccown@engineering.ucla.edu>
2019-05-07 09:39:50 -0700

    updated controller gains, removed display and buttons, changed how printing works

--- eCVT_PID_control/eCVT_PID_control.ino
+++ eCVT_PID_control/eCVT_PID_control.ino
@@ -5,26 +5,7 @@
  * created: 4/27/19 
  */
 
-#include <Wire.h>
 #include <Servo.h>
-
-// display setup
-#include <Wire.h>
-#include <I2C_LCD.h>
-I2C_LCD LCD;
-uint8_t I2C_LCD_ADDRESS = 0x51;
-extern GUI_Bitmap_t bmBruinRacing;
-extern GUI_Bitmap_t bmBearHead;
-const int refreshPeriod = 1000; // display refresh time [ms]
-int lastRefreshTime(0);
-
-// buttons
-const byte button1_pin = 13;
-const byte button2_pin = 12;
-const byte button3_pin = 11;
-const byte button4_pin = 10;
-byte mode = 0;
-char mode_names[2][7] = {"TORQUE", "POWER "};
 
 // PWM constants
 #define PW_STOP 1515
@@ -61,9 +42,9 @@ int u_k(0);
 int u_k1(0);
 const byte controlPeriod = 20; // [ms]
 const double Ts = controlPeriod/1000.0; // controlPeriod [s]
-const double Kp = 14;
+const double Kp = 10;
 const double Ki = 2.744;
-const double Kd = .626;
+const double Kd = 1.252;
 const int N = 100;
 unsigned long lastControlTime(0);
 
@@ -71,7 +52,7 @@ unsigned long lastControlTime(0);
 #define HF_HIGH 800
 #define HF_LOW 100
 bool im_high = false;
-const byte engine_pin = A4;
+const byte engine_pin = A3;
 unsigned long engine_trigger_time(0);
 unsigned long engine_last_trigger(0);
 unsigned int engine_rpm(0);  
@@ -85,6 +66,7 @@ unsigned int gearbox_rpm(0);
 // brake sensor
 const byte brake_pin = 8;
 const int u_k_brake = 311;
+bool brake(false);
 
 // moving average filters
 const size_t num_readings = 4;
@@ -103,21 +85,6 @@ void setup() {
   // open serial connection
   Serial.begin(9600);
 
-  // init I2C interface
-//   Wire.begin();
-//   LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
-
-  // clear screen and display sweaty bruin
-//  LCD.CleanAll(WHITE);
-//  LCD.DrawScreenAreaAt(&bmBruinRacing, 0, 1);
-//  delay(2000);
-  
-  // setup buttons
-  pinMode(button1_pin, INPUT);
-  pinMode(button2_pin, INPUT);
-  pinMode(button3_pin, INPUT);
-  pinMode(button4_pin, INPUT);
-
   // setup actuator
   Actuator.attach(actuator_pin);
   Actuator.writeMicroseconds(PW_STOP);
@@ -127,23 +94,12 @@ void setup() {
   // setup engine sensor
   pinMode(engine_pin, INPUT);
   init_readings(engine_readings);
-  engine_last_trigger = millis();
 
   // setup gearbox sensor
   pinMode(gearbox_pin, INPUT);
   init_readings(gearbox_readings);
-  gearbox_last_trigger = millis();
   attachInterrupt(digitalPinToInterrupt(gearbox_pin), gearbox_isr, CHANGE);
   interrupts();
-
-  // configure LCD to write text
-//  LCD.CleanAll(WHITE);
-//  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
-//  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
-//  LCD.FontModeConf(Font_6x8, FM_MNL_AAA, BLACK_NO_BAC);
-//  LCD.DispStringAt("MODE:", 68, 5);
-//  LCD.DispStringAt("RPM:", 68, 35);
-//  LCD.FontModeConf(Font_8x16_2, FM_MNL_AAA, BLACK_BAC);
 
   // create timer interrupt
   OCR0A = 0xAF;
@@ -156,29 +112,23 @@ SIGNAL(TIMER0_COMPA_vect) {
     control_function();
     lastControlTime = current_millis;
 
-    Serial.print(r_k);
-     Serial.print(" ");
-     Serial.print(u_k);
-    // Serial.print(" ");
-    // Serial.print(engine_rpm);
-    Serial.print(" ");
-    Serial.print(engine_rpm_ave);
-//     Serial.print(" ");
-//     Serial.print(gearbox_rpm);
-//    Serial.print(" ");
-//    Serial.print(gearbox_rpm_ave);
-    Serial.print(" ");
-    Serial.print(current_pos);
-    Serial.print(" ");
-    Serial.print(digitalRead(brake_pin));
-    // Serial.print(" ");
-    // Serial.print(millis());
-    Serial.print("\n");
+    String output = 
+    // r_k
+    // + " " + u_k
+    // + " " + engine_rpm
+    + " " + engine_rpm_ave
+    // + " " + gearbox_rpm
+    + " " + gearbox_rpm_ave
+    // + " " + current_pos
+    // + " " + brake
+    // + " " + millis()
+    Serial.println(output);
   }
 }
 
 void gearbox_isr() {
   gearbox_trigger_time = millis();
+  // rpm to mph .068425
   gearbox_rpm = 5510.204 / (gearbox_trigger_time - gearbox_last_trigger);
   gearbox_readings[gearbox_index] = gearbox_rpm;
   gearbox_index = (gearbox_index + 1) % num_readings;
@@ -201,39 +151,40 @@ unsigned int rpm_average(const unsigned int* readings) {
 
 void control_function() {
 
-  // calculate engine rpm
-  engine_rpm_ave = rpm_average(engine_readings);
-
   // calculate gearboxrpm
   gearbox_rpm_ave = rpm_average(gearbox_readings);
 
-  // check brake
-  bool brake = digitalRead(brake_pin);
-
-// adjust reference
-//  if (engine_rpm_ave < THRESH_LOW) {
-//    r_k = MAX_TORQUE;
-//  } else if (engine_rpm_ave > THRESH_HIGH) {
-//    r_k = MAX_POWER;
-//  }
+  // adjust reference
+  if (engine_rpm_ave < THRESH_LOW) {
+    r_k = MAX_TORQUE;
+  } else if (engine_rpm_ave > THRESH_HIGH) {
+    r_k = MAX_POWER;
+  }
   
+  // calculate engine rpm
+  engine_rpm_ave = rpm_average(engine_readings);
+
   // compute error
   e_k = r_k - engine_rpm_ave;
   e_k_sum = constrain(e_k_sum + e_k, u_k_min, u_k_max);
+
+  // check brake
+  brake = digitalRead(brake_pin);
 
   // compute control signal
   u_k = Kp*e_k + Ki*Ts/2*e_k_sum + u_k1 + Kd*N*(e_k-e_k1)-(N*Ts-1)*u_k1 + u_k_brake*brake;
   u_k = constrain(u_k, u_k_min, u_k_max);
 
+  // force shift out if rpm is too low
   if (engine_rpm_ave < 1000) {
     u_k = u_k_max;
   }
 
-  // check actuator limits
+  // software limit switches
   if (current_pos >= POT_ENGAGE) {
-   u_k = -u_k_limit;
+    u_k = -u_k_limit;
   } else if (current_pos <= POT_MIN) {
-   u_k = u_k_limit;
+    u_k = u_k_limit;
   }
 
   // write to actuator
@@ -242,21 +193,6 @@ void control_function() {
   // update past values
   e_k1 = e_k;
   u_k1 = u_k;
-  
-}
-
-void update_display() {
-  LCD.WorkingModeConf(OFF, ON, WM_BitmapMode);
-  LCD.DrawScreenAreaAt(&bmBearHead, 0, 0);
-  LCD.WorkingModeConf(OFF, ON, WM_CharMode);
-  LCD.FontModeConf(Font_6x8, FM_MNL_AAA, BLACK_NO_BAC);
-  LCD.DispStringAt("MODE:", 68, 5);
-  LCD.DispStringAt("RPM:", 68, 35);
-  LCD.FontModeConf(Font_8x16_2, FM_MNL_AAA, BLACK_BAC);
-  LCD.DispStringAt(mode_names[mode], 78, 15);
-  char rpm_str[5];
-  itoa(engine_rpm, rpm_str, 10);
-  LCD.DispStringAt(rpm_str, 78, 45);
 }
 
 void loop() {
@@ -268,7 +204,7 @@ void loop() {
   int reading = analogRead(engine_pin);
   if (reading > HF_HIGH) {
     im_high = true;
-  } 
+  }
   if (im_high && (reading < HF_LOW)) {
     engine_trigger_time = millis();
     engine_rpm = 60000.0 / (engine_trigger_time - engine_last_trigger);
@@ -277,22 +213,4 @@ void loop() {
     engine_last_trigger = engine_trigger_time;
     im_high = false;
   }
-
-  // check button presses
-   if (digitalRead(button1_pin) == LOW) {
-     r_k = MAX_TORQUE;
-     mode = 0;
-   }
-   if (digitalRead(button2_pin) == LOW) {
-     r_k = MAX_POWER;
-     mode = 1;
-   }
-
-  // refresh display
-  int current_millis = millis();
-  if (current_millis - lastRefreshTime >= refreshPeriod) {
-//    update_display();
-    lastRefreshTime = current_millis;
-  }
-
 }
